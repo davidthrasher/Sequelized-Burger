1905c7a45e035e1ac27af9d81ece912d77e1cfac
David Thrasher <thrasher1332@hotmail.com>
2017-08-21 21:54:18 -0400

    Almost Working

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var router = express.Router();
+var burger = require("../models/burger.js");
+
+// index page
+router.get('/', function (req, res) {
+  burger.selectAll(function(data) {
+    var cheeseBurgers = { burgers: data };
+    res.render('index', cheeseBurgers);
+  });
+});
+// create a new burger
+router.post('/burger/create', function (req, res) {
+  burger.insertOne(req.body.burger_name, function() {
+    res.redirect('/');
+  });
+});
+// devour a burger
+router.post('/burger/eat/:id', function (req, res) {
+  burger.updateOne(req.params.id, function() {
+    res.redirect('/');
+  });
+});
+module.exports = router;
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers(burger_name) VALUES ('Krispy Kreme Burger');
+INSERT INTO burgers(burger_name) VALUES ('Double Coronary Bypass Burger');
+INSERT INTO burgers(burger_name) VALUES ('The Pickle Rick Burger');
